c8be94fc6a9d9e7710faeb0a240841240706571b
ralfxdev <lopezalexander1019@gmail.com>
2024-02-29 00:37:53 -0600

    add string concatenation

---
+++ string-concatenation/Program.cs
@@ -0,0 +1,21 @@
+﻿string str1 = "Hola";
+string str2 = "Mundo";
+
+Console.WriteLine($"Concatenación de Strings");
+
+//Operador +
+string r1 = str1 + " " + str2;
+Console.WriteLine($"Operador + -> {r1}");
+
+//Método String.Concat()
+string r2 = string.Concat(str1, " ", str2);
+Console.WriteLine($"String.Concat() -> {r2}");
+
+//Interpolación de cadenas
+string r3 = $"{str1} {str2}";
+Console.WriteLine($"Interpolación -> {r3}");
+
+//Método String.Join()
+string[] words = { $"{str1}", $"{str2}" };
+string r4 = string.Join(" ", words);
+Console.WriteLine($"String.Join() -> {r4}");
