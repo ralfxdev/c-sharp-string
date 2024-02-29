10ab45b28cca3001c26e642a2ae41eeeb58879a1
ralfxdev <lopezalexander1019@gmail.com>
2024-02-28 23:47:48 -0600

    add string literal

---
+++ string-literal/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hola Mundo!");
+string path = @"C:\temp";
+Console.WriteLine($"{path}");
+string literal = @"Hola Mundo,esta es una cadena Literal";
+Console.WriteLine($"{literal}");
